b0b2a510528ffbe51e1de0ca99f892d76fe82f6c
MarkAC007 <56160609+MarkAC007@users.noreply.github.com>
2023-05-22 20:52:06 +0100

    added_new

--- main.py
+++ main.py
@@ -29,34 +29,13 @@ def generate_text(prompt):
     )
     return response.choices[0].text.strip()
 
-STABILITYAI_API_KEY = os.environ.get('STABILITYAI_API_KEY')
-
-engine_id = "stable-diffusion-v1-5"
-
 def generate_image_description(prompt):
-    response = requests.post(
-        f"https://api.stability.ai/v1/generation/{engine_id}/text-to-image",
-        headers = {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-            "Authorization": f"Bearer {STABILITYAI_API_KEY}"
-        },
-        json={
-            "text_prompts": [
-                {
-                    "text": prompt
-                }
-            ]
-        }
+    response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size="1024x1024"
     )
-    response_data = response.json()
-    if response.status_code == 200 and "data" in response_data:
-        return response_data["data"][0]["url"]
-    else:
-        error_message = response_data.get('message', 'Unknown error')
-        logger.error(f"Error generating image: {error_message} (Status code: {response.status_code})")
-        return None
-
+    return response['data'][0]['url']
 
 def save_image_description_to_file(image_description, output_file="prompts.txt"):
     with open(output_file, "a") as file:
@@ -68,11 +47,11 @@ rss_feed_list = parse_rss_feed("rss_feeds.txt")
 for rss_feed in rss_feed_list:
     rss_title, rss_description, rss_url = rss_feed.values()
 
-    article_prompt = f"You are a helpful assistant that summarizes articles about artificial intelligence in a funny and witty way. Start with a bulleted list containing a breakdown of the article's key points, and create closing two paragraphs with your \"<h3>Hot Take</h3>\". Include h1, h2, h3 tags in the final output where appropriate. {rss_description}"
+    article_prompt = f"You are a helpful assistant that summarizes articles about artificial intelligence in a funny and witty way. Start with a bulleted list containing a breakdown of the article's key points, and then create closing two paragraphs with about 500 words, to include your \"<h3>Hot Take</h3>\". Include h1, h2, h3 tags in the final output where appropriate. {rss_description}"
     article_content = generate_text(article_prompt)
     article_content += f"\nTo learn more, check out the source here: {rss_url}"
 
-    image_prompt = f"You are a helpful assistant that creates images based on articles titles. Create a brief visual description of what an image would look like for the title: {rss_title}"
+    image_prompt = f"You are a helpful assistant that creates images based on articles titles. Create a midjourney prompt for what an image would look like for the title: {rss_title}"
     image_description = generate_text(image_prompt)
 
     # Save image_description to a text file
